30b2420a29eed1b28445d950d678284650f966fa
FrederickIge <duelarm@gmail.com>
2018-10-24 17:37:04 -0400

    cleaned up landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import "./css/App.css";
-
-import "./css/App.css";
-import Navbar from "./layout/Navbar";
-
-import Landingpage from "./pages/Landingpage";
-
 import posed, { PoseGroup } from 'react-pose';
+import { Switch, Route } from 'react-router-dom';
+
 import withAuthentication from './components/hoc/withAuthentication';
+import Navbar from './layout/Navbar';
 
-
-import AccountPage from "./pages/account/Account"
-import Passwordforget from "./pages/account/Passwordforget"
-import Passwordchange from "./pages/account/Passwordchange"
-import Login from "./pages/account/Login";
-import Signup from "./pages/account/Signup";
-
-import Dashboard from "./pages/dogs/Dashboard";
+import Landingpage from './pages/Landingpage';
+import AccountPage from './pages/account/Account';
+import Login from './pages/account/Login';
+import Signup from './pages/account/Signup';
+import Dashboard from './pages/dogs/Dashboard';
 import DogList from './pages/dogs/DogList';
 
-import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
-import { Switch, Route } from "react-router-dom";
+import './css/App.css';
 
 const RoutesContainer = posed.div({
   enter: {
@@ -33,32 +25,26 @@ const RoutesContainer = posed.div({
 
 class App extends Component {
   render() {
-  
     return (
-      <Route render={({ location }) => (
-
-        <div>
-          <Navbar />
-          <PoseGroup>
-            <RoutesContainer key={location.pathname} >
-              <Switch location={location}   >
-
-              
-                <Route exact path="/login" component = {Login} />
-                <Route exact path="/signup" component = {Signup} />
-                <Route exact path="/account" component = {AccountPage} />
-                <Route exact path="/dashboard" component = {Dashboard} />
-                <Route exact path="/doglist" component = {DogList} />
-                <Route path="/" component = {Landingpage} />
-
-              </Switch>
-            </RoutesContainer>
-          </PoseGroup>
-        </div>
-
-      )}>
-
-      </Route>
+      <Route
+        render={({ location }) => (
+          <div>
+            <Navbar />
+            <PoseGroup>
+              <RoutesContainer key={location.pathname}>
+                <Switch location={location}>
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/signup" component={Signup} />
+                  <Route exact path="/account" component={AccountPage} />
+                  <Route exact path="/dashboard" component={Dashboard} />
+                  <Route exact path="/doglist" component={DogList} />
+                  <Route path="/" component={Landingpage} />
+                </Switch>
+              </RoutesContainer>
+            </PoseGroup>
+          </div>
+        )}
+      />
     );
   }
 }
--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -1,106 +1,88 @@
-import React, { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faSquare, faFire } from "@fortawesome/free-solid-svg-icons";
-import { faReact, faCss3Alt } from "@fortawesome/free-brands-svg-icons";
-import { Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFire } from '@fortawesome/free-solid-svg-icons';
+import { faReact, faCss3Alt } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
-class Myresults extends Component{
-  componentDidMount(){
-    // this.props.history.push('/dashboard')
-  }
-    render(){
-       return(
-            <div>
-            <div>
-           
-              {/* Masthead */}
-              <header className="masthead text-white text-center">
-                <div className="overlay" />
-                <div className="container">
-                  <div className="row">
-                    <div className="col-xl-9 mx-auto">
-                      <h1 className="mb-5 text-secondary">
-                       Welcome Doggie Boiler
-                      </h1>
+class Myresults extends Component {
+  render() {
+    const HeroSection = () => {
+      return (
+        <header className="masthead text-white text-center">
+          <div className="overlay" />
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-9 mx-auto">
+                <h1 className="mb-5 text-secondary">Welcome to Doggie Boiler</h1>
+              </div>
+              <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                <form>
+                  <div className="form-row">
+                    <div className="col-12 col-md-6 mb-2 mb-md-0">
+                      <Link to="/signup" className="btn btn-block btn-lg btn-primary">
+                        Sign up!
+                      </Link>
                     </div>
-                    <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
-                      <form>
-                        <div className="form-row">
 
-                          <div className="col-12 col-md-6 mb-2 mb-md-0">
-               
-                                 <Link to="/signup"  className="btn btn-block btn-lg btn-primary" >Sign up!</Link> 
-                             
-                         
-                          </div>
-
-                          <div className="col-12 col-md-6">
-                          <Link to="/login"  className="btn btn-block btn-lg btn-primary" >Login</Link>
-                          </div>
-
-
-                        </div>
-                      </form>
+                    <div className="col-12 col-md-6">
+                      <Link to="/login" className="btn btn-block btn-lg btn-primary">
+                        Login
+                      </Link>
                     </div>
                   </div>
-                </div>
-              </header>
-              {/* Icons Grid */}
-              <section className="features-icons  text-center">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-lg-4">
-                      <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3 p-3 mb-5 bg-white rounded hvr-grow-shadow">
-                        <div className="features-icons-icon d-flex">
-                          <FontAwesomeIcon
-                            className="icon-layers m-auto text-info"
-                            icon={faCss3Alt}
-                          />
-                        </div>
-                        <h3>Bootstrap 4</h3>
-                        <p className="lead mb-0">
-                          The world's most popular CSS framework
-                        </p>
-                      </div>
-                    </div>
-                    <div className="col-lg-4">
-                      <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3 p-3 mb-5 bg-white rounded hvr-grow-shadow">
-                        <div className="features-icons-icon d-flex">
-                          <FontAwesomeIcon
-                            className="icon-layers m-auto text-info"
-                            icon={faFire}
-                          />
-                        </div>
-                        <h3>Firebase</h3>
-                        <p className="lead mb-0  text-default">
-                          Backend Database and Auth powered by Firebase
-                        </p>
-                      </div>
-                    </div>
-                    <div className="col-lg-4">
-                      <div className="features-icons-item mx-auto mb-0 mb-lg-3 p-3 mb-5 bg-white rounded hvr-grow-shadow">
-                        <div className="features-icons-icon d-flex">
-                          <FontAwesomeIcon
-                            className="icon-layers m-auto text-info"
-                            icon={faReact}
-                          />
-                        </div>
-                        <h3>Create React App</h3>
-                        <p className="lead mb-0 text-default">
-                          The world's most popular JS framework + starter kit
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-      
-            
-           
+                </form>
+              </div>
             </div>
           </div>
-        )
-    }
+        </header>
+      );
+    };
+
+    const FeaturesSection = () => {
+      return (
+        <section className="features-icons  text-center">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4">
+                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3 p-3 mb-5 bg-white rounded hvr-grow-shadow">
+                  <div className="features-icons-icon d-flex">
+                    <FontAwesomeIcon className="icon-layers m-auto text-info" icon={faCss3Alt} />
+                  </div>
+                  <h3>Bootstrap 4</h3>
+                  <p className="lead mb-0">The world's most popular CSS framework</p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3 p-3 mb-5 bg-white rounded hvr-grow-shadow">
+                  <div className="features-icons-icon d-flex">
+                    <FontAwesomeIcon className="icon-layers m-auto text-info" icon={faFire} />
+                  </div>
+                  <h3>Firebase</h3>
+                  <p className="lead mb-0  text-default">Backend Database and Auth powered by Firebase</p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="features-icons-item mx-auto mb-0 mb-lg-3 p-3 mb-5 bg-white rounded hvr-grow-shadow">
+                  <div className="features-icons-icon d-flex">
+                    <FontAwesomeIcon className="icon-layers m-auto text-info" icon={faReact} />
+                  </div>
+                  <h3>Create React App</h3>
+                  <p className="lead mb-0 text-default">The world's most popular JS framework + starter kit</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      );
+    };
+
+    return (
+      <div>
+        <HeroSection />
+        <FeaturesSection />
+      </div>
+    );
+  }
 }
 
-export default Myresults
+export default Myresults;
