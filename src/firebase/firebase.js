7f322860956d3135e1cc613d3284413875fde48b
FrederickIge <duelarm@gmail.com>
2018-10-27 12:06:41 -0400

    add two firebase env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,41 +3,52 @@ import 'firebase/auth';
 import * as firebaseui from 'firebaseui';
 
 let loginUi = null;
+let config = null;
 
-const config = {
-  apiKey: "AIzaSyCuUSuEVo_s9IBall-MpUvDQMdcSLmx7BI",
-  authDomain: "react-fire-9a99e.firebaseapp.com",
-  databaseURL: "https://react-fire-9a99e.firebaseio.com",
-  projectId: "react-fire-9a99e",
-  storageBucket: "react-fire-9a99e.appspot.com",
-  messagingSenderId: "528027897230"
+const prodconfig = {
+  apiKey: 'AIzaSyCuUSuEVo_s9IBall-MpUvDQMdcSLmx7BI',
+  authDomain: 'react-fire-9a99e.firebaseapp.com',
+  databaseURL: 'https://react-fire-9a99e.firebaseio.com',
+  projectId: 'react-fire-9a99e',
+  storageBucket: 'react-fire-9a99e.appspot.com',
+  messagingSenderId: '528027897230'
+};
+
+const devconfig = {
+  apiKey: 'AIzaSyCtkGCQVNHOxKgKYgDeBeYPaJiYDoYa9uo',
+  authDomain: 'doggietime-prod.firebaseapp.com',
+  databaseURL: 'https://doggietime-prod.firebaseio.com',
+  projectId: 'doggietime-prod',
+  storageBucket: '',
+  messagingSenderId: '1088180833349'
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config = prodconfig;
+} else {
+  config = devconfig;
 }
 
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
-  signInFlow: "popup",
+  signInFlow: 'popup',
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: "/searchschools",
+  signInSuccessUrl: '/searchschools',
   // We will display Google and Facebook as auth providers.
   signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   callbacks: {
     // Avoid redirects after sign-in.
-    signInSuccessWithAuthResult: data => {
-   
-      this.props.history.push("/searchschools");
+    signInSuccessWithAuthResult: (data) => {
+      this.props.history.push('/searchschools');
     }
   }
 };
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(config);
-     loginUi = new firebaseui.auth.AuthUI(firebase.auth());
-  }
+  firebase.initializeApp(config);
+  loginUi = new firebaseui.auth.AuthUI(firebase.auth());
+}
 
-  const auth = firebase.auth();
+const auth = firebase.auth();
 
-export {
-  auth,
-  loginUi,
-  uiConfig
-};
+export {auth, loginUi, uiConfig};
