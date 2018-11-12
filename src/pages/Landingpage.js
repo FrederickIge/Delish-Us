ff712a3fb3637f8966960073f0c205dd42cf1fba
FrederickIge <duelarm@gmail.com>
2018-11-12 11:02:49 -0500

    basic filtering

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -12,14 +12,16 @@ import { Link } from "react-router-dom";
 import { africa } from "../img/africa.png";
 import axios from 'axios'
 import posed from 'react-pose';
-import {inject, observer} from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 
 const Slide = posed.div({
-  enter: {x: 0, opacity: 1},
-  exit: {x: -50, opacity: 0}
+  enter: { x: 0, opacity: 1 },
+  exit: { x: -50, opacity: 0 }
 });
 
-@inject('routingStore','sessionStore')
+let fullList = []
+
+@inject('routingStore', 'sessionStore')
 @observer
 class Myresults extends Component {
 
@@ -35,16 +37,30 @@ class Myresults extends Component {
   ];
 
   state = {
-    foodList: []
+    foodList: [],
+    category: 'All'
   };
 
-  goToFood = (id) =>{
+  goToFood = (id) => {
     this.routingStore.push("/food/" + id);
+  }
+  onChange = (event) => {
+
+   let filteredList = fullList.filter(food => food.category == event.target.value)
+   
+
+
+    this.setState({
+      category: event.target.value,
+      foodList: filteredList
+    })
+
   }
 
   componentWillMount() {
     axios.get('list.json').then(res => {
-      console.log(res.data)
+     
+       fullList = res.data
       this.setState({ foodList: res.data })
     })
   }
@@ -53,13 +69,9 @@ class Myresults extends Component {
 
     return (
       <div>
-        {/* <HeroSection list = {this.state.foodList} /> */}
-
-
         <header className="masthead text-white text-center">
           <div className="overlay" />
           <div className="container">
-
             <div className="row">
               <div className="col-xl-9 mx-auto">
                 <h1 className="mb-5">The Sebi Diet Food Database</h1>
@@ -72,104 +84,68 @@ class Myresults extends Component {
               </div>
             </div>
           </div>
+        </header>
 
           <div className="container">
-            <div className="row justify-content-center">
-             
-              <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                <li class="nav-item">
-                  <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">All</a>
-                </li>
+            <div className="justify-content-center">
 
-                <li class="nav-item">
-                  <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Vegetables</a>
-                </li>
-
-                <li class="nav-item">
-                  <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Fruits</a>
-                </li>
-
-                <li class="nav-item">
-                  <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Spices</a>
-                </li>
-
-                <li class="nav-item">
-                  <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Grains</a>
-                </li>
-
-                <li class="nav-item">
-                  <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Sugars</a>
-                </li>
-
-                <li class="nav-item">
-                  <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Teas</a>
-                </li>
-
-                <li class="nav-item">
-                  <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Herbs</a>
-                </li>
-
-              </ul>
-            
-
+              <div className="form-group">
+                <label>Food Category</label>
+                <select value={this.state.category} onChange={this.onChange} className="form-control" >
+                  <option value="All Foods">All Foods</option>
+                  <option value="Vegetables">Vegetables</option>
+                  <option value="Fruits">Fruits</option>   
+                  <option value="Alkaline Grains">Alkaline Grains</option>      
+                  <option value="Spices and Seasonings">Spices and Seasonings</option>
+                  <option value="Alkaline Sugars and Sweeteners">Alkaline Sugars and Sweeteners</option>
+                  <option value="Herbal Teas">Herbal Teas</option>
+                  <option value="Herb">Herb</option>
+                
+                </select>
+                
+              </div>
 
             </div>
           </div>
+
+        <br></br>
+
           <div className="container-fluid">
-            {/* <div className="row">
-              {this.array.map(item => (
-                <div key="item" className="col-sm-12 col-md-4">
-                  <div className="features-icons-item mx-auto mb-0 mb-lg-3 p-3 mb-5  rounded ">
-                    <div className="food-category-container hvr-grow-shadow shadow  ">
-                      <img
-                        className=" africa-img"
-                        src={require("../img/africa.png")}
-                        alt="lol"
-                      />
-
-                      <div className="food-category mx-auto">
+            <Slide>
+              <div className="row">
+                {this.state.foodList.map(food => (
+                  <div key={food.id} onClick={() => this.goToFood(food.id)} className=" col-sm-6 col-md-6 col-lg-4 col-xl-3">
+                    <div className="features-icons-item mx-auto rounded d-flex justify-content-center">
+                      <div className="food-category-container hvr-grow-shadow shadow">
                         <img
-                          className="img-fluid category-img"
-                          src={require("../img/categories/" + item + ".png")}
+                          className="africa-img"
+                          src={require("../img/africa.png")}
                           alt="lol"
                         />
-                      </div>
-                    </div>
-                    <br />
-                    <h3>{item}</h3>
-                  </div>
-                </div>
-              ))}
-            </div> */}
-<Slide>
-            <div className="row">
-              {this.state.foodList.map(food => (
-                <div key={food.id} onClick={() => this.goToFood(food.id) } className="col-sm-6 col-md-4 col-lg-4 col-xl-3">
-                  <div className="features-icons-item mx-auto mb-0 mb-lg-3 p-3 mb-5 rounded ">
-                    <div className="food-category-container hvr-grow-shadow shadow">
-                      <img
-                        className="africa-img"
-                        src={require("../img/africa.png")}
-                        alt="lol"
-                      />
 
-                      <div className="food-category mx-auto">
-                        <img
-                          className="img-fluid category-img"
-                          src={food.image}
-                          alt="lol"
-                        />
+                        <div className="food-category mx-auto">
+
+                        <div className="category-img">
+                          <img
+                            className="img-fluid "
+                            src={food.image}
+                            alt="lol"
+                          />
+                          <br></br>
+                          <p>{food.name}</p>
+                          </div>
+                        </div>
+                        
                       </div>
+                      <br />
+                      
                     </div>
-                    <br />
-                    <h3>{food.name}</h3>
                   </div>
-                </div>
-              ))}
-            </div>
+                ))}
+              </div>
             </Slide>
           </div>
-        </header>
+       
 
 
       </div>
